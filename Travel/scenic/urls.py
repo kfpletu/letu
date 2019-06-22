c584cc47c7610004e67a2b90ec0f1acdcbfe2f50
kavin-lee <kavin_lee@yeah.net>
2019-06-22 18:50:32 +0800

    gfdgfg

--- Travel/scenic/urls.py
+++ Travel/scenic/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
+from . import views
 
 
 urlpatterns = [
-
+    url(r'^$',views.index,name='scenic1'),
+    url(r'^ticket$',views.ticket,name='ticket'),
+    url(r'^scenic2',views.scenic2,name='scenic2')
 ]
--- Travel/scenic/views.py
+++ Travel/scenic/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Create your views here.
+def index(request):
+    return render(request,'scenic/information.html')
+def ticket(request):
+    return render(request,'scenic/ticket.html')
+def scenic2(request):
+    return render(request,'scenic/information02.html')
