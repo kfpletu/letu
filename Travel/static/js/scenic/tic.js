48b95ee15f14ad23544c5545d3e2dbeba96ef60f
kavin-lee <kavin_lee@yeah.net>
2019-06-22 18:45:50 +0800

    gfgfgd

---
+++ Travel/static/js/scenic/tic.js
@@ -0,0 +1,30 @@
+
+$(document).ready(function() {
+  $('#view').setZoomPicture({
+    thumbsContainer: '#pics-thumbs',
+    prevContainer: '#nav-left-thumbs',
+    nextContainer: '#nav-right-thumbs',
+    // zoomContainer: '#zoom'
+  });
+});
+$(document).ready(function() {
+  $('#view').setZoomPicture({
+    thumbsContainer: '#pics-thumbs',
+    prevContainer: '#nav-left-thumbs',
+    nextContainer: '#nav-right-thumbs',
+    // zoomContainer: '#zoom',
+    zoomLevel: 2
+  });
+});
+
+$(document).ready(function() {
+  $('#view').setZoomPicture({
+    thumbsContainer: '#pics-thumbs',
+    prevContainer: '#nav-left-thumbs',
+    nextContainer: '#nav-right-thumbs',
+    // zoomContainer: '#zoom',
+    // loadMsg: none
+  })
+});
+
+
