d9b3910c3337676445c94a990c8c9111081f2482
Wmh <1308732441@qq.com>
2019-06-22 13:40:44 +0800

    updata user

--- Travel/user/views.py
+++ Travel/user/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'user/login.html')
+        
+
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'user/register.html')
