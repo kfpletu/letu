e738cc550e1082bf439fda5e925b2f82cd01c7f5
Wmh <1308732441@qq.com>
2019-06-24 13:06:05 +0800

    updata

--- Travel/Travel/urls.py
+++ Travel/Travel/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', include("user.urls")),
+    url(r'', include("user.urls")),
     url(r'^user/', include("user.urls")),
     url(r'^scenic/', include("scenic.urls")),
     url(r'^hotel/', include("hotel.urls")),
--- Travel/user/urls.py
+++ Travel/user/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-from django.contrib import admin
+# from django.contrib import admin
 
 from . import views
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    # url(r'^admin/', admin.site.urls),
     url('^$', views.index),
     url(r'^index', views.index),
     url(r'^login', views.login,),
