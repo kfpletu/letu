2775e34e863e577833d64f2d338a3a3f9e5f64e6
Wmh <1308732441@qq.com>
2019-06-24 19:07:40 +0800

    增加登录功能

--- Travel/user/views.py
+++ Travel/user/views.py
@@ -2,18 +2,43 @@ from django.shortcuts import render
 
 # Create your views here.
 from .models import *
+from django.http import HttpResponse
 
 # 登录
 def login(request):
     if request.method == 'GET':
         return render(request, 'user/login.html')
+    elif request.method == 'POST':
+        # 获取登录页面form表单提交的uname和upwd
+        uname = request.POST.get('uname')
+        upwd = request.POST.get('upwd')
+        # 从数据库获取uname和upwd
+        infos = Info.objects.values('uname', 'upwd')
+        # 循环判断输入的uname和upwd是否相等
+        for info in infos:
+            if uname==info['uname'] and upwd==info['upwd']:
+                return render(request,'index.html',locals())
+            else:
+                continue
+        return render(request,'user/login.html')
 
 #注册
 def register(request):
     if request.method == 'GET':
         return render(request, 'user/register.html')
     elif request.method == 'POST':
-        return
+        # 获取用户注册输入的信息
+        uname = request.POST.get('uname')
+        upwd = request.POST.get('upwd')
+        phone = request.POST.get('phone')
+        email = request.POST.get('email')
+        # 获取数据库uname,判断是否重复
+        if Info.objects.filter(uname=uname):
+            return render(request, 'user/register.html', locals())
+        else:
+            newinfo = Info(uname=uname, upwd=upwd, phone=phone, email=email)
+            newinfo.save()
+            return render(request, 'index.html', locals())
 
 #购物车
 def cart(request):
@@ -21,18 +46,5 @@ def cart(request):
 
 
 def index(request):
-    if request.method == 'GET':
-        uname=''
-        return render(request, 'index.html', locals())
-    elif request.method == 'POST':
-        uname = request.POST.get('uname')
-        upwd = request.POST.get('upwd')
-        phone = request.POST.get('phone')
-        email = request.POST.get('email')
-        newinfo = Info(uname=uname, upwd=upwd, phone=phone, email=email)
-        if Info.objects.filter(uname=uname):
-            return render(request, 'index.html', locals())
-        else:
-            newinfo.save()
-            print('uname:',uname)
-            return render(request, 'index.html', locals())
+    uname=''
+    return render(request, 'index.html', locals())
